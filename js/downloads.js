8c4e16831e30dd23d4427e7390eeae8b6875b8a8
NJDaeger <Jingle12345!>
2020-10-29 16:20:17 -0400

    definitely broke stuff

--- js/downloads.js
+++ js/downloads.js
@@ -18,8 +18,26 @@ function doFinish(hovered, attrib) {
 	}, 200, "easeInOutExpo");
 }
 
+function slide(hovered, attrib) {
+	var map = hovered.attr(attrib);
+	var elem = $(".version[" + attrib + "=" + map + "]");
+	// hovered.toggleClass("slideFromRight");
+	if (hovered.hasClass("slideFromRight")) {
+		hovered.toggleClass("slideFromRight", 100);
+		elem.toggleClass("slideFromRight");
+	} else {
+		hovered.toggleClass("slideFromRight");
+		elem.toggleClass("slideFromRight", 100);
+	}
+/*	elem.stop();
+	elem.animate({
+		backgroundColor: "rgba(0,0,0,0)",
+		color: "#D3D3D3"
+	}, 200, "easeInOutExpo");*/
+}
+
 $(document).ready(function() {
-	$(".map-version").hover(function() {doStart($(this), "map");},function() {doFinish($(this), "map");});
+	$(".map-version").hover(function() {slide($(this), "map");},function() {slide($(this), "map");});
 	$(".pack-version").hover(function() {doStart($(this), "pack");},function() {doFinish($(this), "pack");});
 	$(".version").hover(function() {
 		doStart($(this), "pack");
