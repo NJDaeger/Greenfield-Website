91c24f4befaf4964bf3cbbf23167deb239e7e748
NJDaeger <noahdaeger@hotmail.com>
2022-07-08 22:43:10 -0400

    change asterisk into an I

--- src/pages/conditions.jsx
+++ src/pages/conditions.jsx
@@ -48,7 +48,12 @@ const Conditions = () => {
                 {/* <h1 className="text-white">Terms and Conditions</h1> */}
                 <div className="d-flex flex-column col-10 text-white justify-content-center flex-xl-row align-items-center">
                     <div className="d-block col-10 col-xl-5 m-5 p-4 condition-section" id="#content-conditions">
-                        <h3 className="text-center d-flex flex-row">Map &amp; Resourcepack Usage Conditions<Button text="*" outerClass={"asterisk"} dataType="glass clear" click={() => {setShowMsg1(true)}}></Button></h3>
+                        <h3 className="text-center d-flex flex-row">Map &amp; Resourcepack Usage Conditions<Button text={
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
+                                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                                <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+                            </svg>
+                        } outerClass={"asterisk"} dataType="glass clear" click={() => {setShowMsg1(true)}}></Button></h3>
                         <hr />
                         <div>
                             <h5>Map &amp; Resourcepack Usage on a Server</h5>
@@ -78,7 +83,12 @@ const Conditions = () => {
                         </div>
                     </div>
                     <div className="d-block col-10 col-xl-5 m-5 p-4 condition-section" id="#builder-conditions">
-                        <h3 className="text-center d-flex flex-row">Build Member Conditions<Button text="*" outerClass={"asterisk"} dataType="glass clear" click={() => {setShowMsg2(true)}}></Button></h3>
+                        <h3 className="text-center d-flex flex-row">Build Member Conditions<Button text={
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
+                                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                                <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+                            </svg>
+                        } outerClass={"asterisk"} dataType="glass clear" click={() => {setShowMsg2(true)}}></Button></h3>
                         <hr />
                         <div>
                             <h5>Build Server Conditions</h5>
@@ -101,8 +111,13 @@ const Conditions = () => {
                             <span className="ms-3 mb-0 li">Lying in any form or context will not be tolerated.</span>
                             <span className="ms-3 mb-0 li">English is the primary language we communicate with. All staff members are fluent in the English language. It is a requirement that
                                 you are able to understand and communicate with us via chat messages.</span>
-                            <span className="ms-3 mb-0 li">Any media that is purposefully created to be used as official<Button text="*" outerClass={"asterisk d-inline-flex"} dataType="glass clear" click={() => {setShowMsg3(true)}}></Button>
-                                promotional material for the project is owned by the Greenfield Minecraft Project. Any other media that is not official is owned by the creator of said media.</span>
+                            <span className="ms-3 mb-0 li">Any media that is purposefully created to be used as official promotional material for the project is owned by the Greenfield 
+                            Minecraft Project. Any other media that is not official is owned by the creator of said media. <Button text={
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-circle" viewBox="0 0 16 16">
+                                <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                                <path d="m8.93 6.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM9 4.5a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+                            </svg>
+                        } outerClass={"asterisk d-inline-flex"} dataType="glass clear" click={() => {setShowMsg3(true)}}></Button></span>
                         </div>
                     </div>
                 </div>
