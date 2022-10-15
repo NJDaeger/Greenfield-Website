f37208b1d60b5f720e456885de8dcabdb6c54266
NJDaeger <noahdaeger@hotmail.com>
2022-10-15 09:53:32 -0400

    update link and fix a few minor issues

--- src/pages/downloads.jsx
+++ src/pages/downloads.jsx
@@ -26,14 +26,16 @@ const Downloads = () => {
                 src={stack}
                 focus={<span style={{textShadow:"0 0 2px black, 0 0 10px black"}}>Download Greenfield {getVersionSpan()}</span>}
                 thirdRowContent={
-                    <div className="animateThird d-flex download-buttons hidden align-items-center showing flex-column flex-lg-row w-100" style={{maxWidth:"80vw"}}>
-                        <div className="d-flex flex-column align-items-center w-75">
-                            <Button text="Download Greenfield" dataType="glass" innerClass={"py-1"} outerClass={"m-3 w-75 px-3"} href={downloads.map[downloads.map.length - 1].download ?? "#"}></Button>
-                            <a href={GetPageLink("./project")} className="swift-link fs-5 link text-center" style={{lineHeight:"normal"}}>Download Older Greenfield Versions</a>
-                        </div>
-                        <div className="d-flex flex-column align-items-center w-75">
-                            <Button text="Download Resourcepack" dataType="glass" innerClass={"py-1"} outerClass={"m-3 w-75 px-3"} href={downloads.pack[downloads.pack.length - 1].download ?? "#"}></Button>
-                            <a href={GetPageLink("./resourcepack")} className="swift-link fs-5 link text-center" style={{lineHeight:"normal"}}>Download Older Resourcepack Versions</a>
+                    <div className="animateThird col-11 col-md-10 col-lg-8 d-flex download-buttons hidden justify-content-center showing flex-column">
+                        <div className="d-flex w-100 flex-column flex-lg-row">
+                            <div className="d-flex flex-column align-items-center w-100 mb-4 m-lg-2">
+                                <Button text="Download Greenfield" dataType="glass" innerClass={""} outerClass={"w-100 mb-2"} href={downloads.map[downloads.map.length - 1].download ?? "#"}></Button>
+                                <a href={GetPageLink("./project")} className="swift-link fs-5 link text-center" style={{lineHeight:"normal"}}>Download Older Greenfield Versions</a>
+                            </div>
+                            <div className="d-flex flex-column align-items-center w-100 mb-4 m-lg-2">
+                                <Button text="Download Resourcepack" dataType="glass" innerClass={""} outerClass={"w-100 mb-2"} href={downloads.pack[downloads.pack.length - 1].download ?? "#"}></Button>
+                                <a href={GetPageLink("./resourcepack")} className="swift-link fs-5 link text-center" style={{lineHeight:"normal"}}>Download Older Resourcepack Versions</a>
+                            </div>
                         </div>
                     </div>
                 }
